085a83b220465a1853354bdc120f563bb7450640
crespogianina <crespogianina98@gmail.com>
2022-10-24 19:11:00 -0300

    última tarea

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,7 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  return Object.entries(objeto);
 }
 
 
@@ -18,7 +19,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var resultado = {};
+  var indices= [];
+  for(var i = 0; i < string.length;i++) {
+    if(resultado[string[i]]=== undefined) {
+      resultado[string[i]] = 1;
+    }else {
+      resultado[string[i]]++;
+    }
+  }
+  return resultado;
 }
+
+
 
 
 function capToFront(s) {
@@ -26,6 +39,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let palabras = s.split('');
+  let nuevaPalabra='';
+  let otra='';
+  for(let i =0; i < palabras.length;i++) {
+    if(palabras[i].toUpperCase() === palabras[i]) {
+      nuevaPalabra +=palabras[i];
+    }else {
+      otra +=palabras[i];
+    }
+  }
+  return nuevaPalabra + otra;
 }
 
 
@@ -35,6 +59,15 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+ // "The Henry Challenge is close!" -->"ehT yrneH egnellahC si !esolc" 
+ //"The', 'Henry', 'Challenge', 'is', 'close!" --> "ehT', 'yrneH', 'egnellahC', 'si', '!esolc"
+
+ var palabra= str.split(' ');
+ let alReves = ''; 
+ for(var i= 0; i < palabra.length;i++) {
+  alReves= alReves + ' ' + palabra[i].split('').reverse().join('');
+ }
+ return alReves.trimStart()
 } 
 
 
@@ -43,6 +76,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  let numeritos = numero.toString().split(''); //---> '1','2','5','5','5'
+  let otro = numeritos.slice().reverse() ///
+  if (numeritos.join() === otro.join()) {
+    return 'Es capicua'
+  }else {
+    return 'No es capicua';
+  }
 }
 
 
@@ -50,6 +90,14 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  let palabra = cadena.split('');
+  let nuevaCadena = '';
+  for (let i = 0;i<palabra.length;i++) {
+    if (palabra[i] !== 'a' && palabra[i] !== 'b'&& palabra[i] !== 'c' ) {
+      nuevaCadena += palabra[i]
+    }
+  }
+  return nuevaCadena;
 }
 
 
@@ -57,7 +105,22 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  // let nuevaCadena= [];
+  // arr.map(elemento => { if (elemento.length < )})
+  for(var i = 0; i < arr.length; ++i) {
+    for(var j = i + 1; j < arr.length; ++j) {
+        
+        if(arr[i].length > arr[j].length) {
+            var auxiliar = arr[i];
+            arr[i] = arr[j];
+            arr[j] = auxiliar;
+        }
+    }
 }
+
+return arr;
+}
+
 
 
 function buscoInterseccion(arreglo1, arreglo2){
@@ -66,6 +129,11 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  let intersección = [];
+  for(let i = 0; i < 5; i++) {
+    arreglo1.filter(elemento => { if (elemento === arreglo2[i]) intersección.push(elemento)})
+  }
+  return intersección;
 }
 
 
