302e7cc9807a0979966c867b887ecb67a8179c6c
crespogianina <crespogianina98@gmail.com>
2022-10-25 11:52:20 -0300

    corrección

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -109,16 +109,14 @@ function sortArray(arr) {
   // arr.map(elemento => { if (elemento.length < )})
   for(var i = 0; i < arr.length; ++i) {
     for(var j = i + 1; j < arr.length; ++j) {
-        
-        if(arr[i].length > arr[j].length) {
-            var auxiliar = arr[i];
-            arr[i] = arr[j];
-            arr[j] = auxiliar;
-        }
+      if(arr[i].length > arr[j].length) {
+        var auxiliar = arr[i];
+        arr[i] = arr[j];
+        arr[j] = auxiliar;
+      }
     }
-}
-
-return arr;
+  }
+  return arr;
 }
 
 
